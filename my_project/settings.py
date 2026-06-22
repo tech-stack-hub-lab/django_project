7940cfc943ed605239d25cec7d62f61366f20bbc
tech-stack-hub-lab <533702@waes.ac.uk>
2026-06-22 17:46:45 +0100

    add debug false for production

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_lx+@((qqwxbawckiac0l0yrl2do()om@=pu7%u!(%a)x#ic9r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
