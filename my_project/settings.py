e62ce6ac2a2ce73af0e985f094a6755a198eaaf0
tech-stack-hub-lab <533702@waes.ac.uk>
2026-06-22 17:17:23 +0100

    deploy code in heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_lx+@((qqwxbawckiac0l0yrl2do()om@=pu7%u!(%a)x#ic9r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
